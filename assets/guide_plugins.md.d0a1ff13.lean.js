e620e55d20ac65f5cd37e86cd4c4be19bdbabeb9
kingyue737 <kingyue737@users.noreply.github.com>
2023-01-12 06:02:42 +0000

    deploy: 401c91777b34bf8fd96b503e8e8717f4fbdd3e9e

---
+++ assets/guide_plugins.md.d0a1ff13.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as n,o as a,a as l}from"./app.fe018d69.js";const u=JSON.parse('{"title":"Plugins","description":"","frontmatter":{},"headers":[],"relativePath":"guide/plugins.md","lastUpdated":1673503299000}'),p={name:"guide/plugins.md"},o=l("",7),e=[o];function t(c,r,y,i,A,D){return a(),n("div",null,e)}const E=s(p,[["render",t]]);export{u as __pageData,E as default};
